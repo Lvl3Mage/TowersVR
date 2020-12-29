28e602f0f8331cd54911463ed7231380c58160e4
Lvl3Mage <lvlthreemage@gmail.com>
2020-12-29 02:34:22 +0100

    Reworked Destructibility
    Object destructibility is now object-oriented and very modular.
    
    + played around with some outline shading

---
+++ Towers_VR/Assets/DestructableObject.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableObject : MonoBehaviour
+{
+	[SerializeField] protected float _Health = 0;
+	[SerializeField] bool OutsideDamage = true;
+	[SerializeField] float OutsideDamageMultiplier = 1;
+	[SerializeField] bool PhysicsDamage = true;
+	[SerializeField] float VelocityDamageMultiplier = 1, OtherMassDamageMultiplier = 1, PhysicsDamageThreshold = 0;
+	public float Health
+	{
+		get{
+			return _Health;
+		}
+	}
+    public void DamageObject(float Damage){
+    	if(OutsideDamage){
+ 			ApplyDamage(Damage*OutsideDamageMultiplier);
+    	}
+    }
+    protected virtual void OnCollisionEnter(Collision collisionInfo){
+    	if(PhysicsDamage){
+    		Rigidbody ColidedRigidbody = collisionInfo.rigidbody; // refers to the rigidbody we've collided with
+    		float VelocityDamage = collisionInfo.relativeVelocity.magnitude* VelocityDamageMultiplier;
+    		float MassDamage;
+    		if(ColidedRigidbody){ // if colided with a rigidbody then it calculates the mass damage, but if the cillision occured with a static object the mass famage is set to 1 (it doesn't depend on mass in this case)
+    			MassDamage = ColidedRigidbody.mass * OtherMassDamageMultiplier;
+    		}
+    		else{
+    			MassDamage = 1;
+    		}
+
+    		float Damage = VelocityDamage * MassDamage; // This can be done in three multiplication but I am keeping the multipliers separate because it makes more sence this way
+    		if(Damage > PhysicsDamageThreshold){
+    			ApplyDamage(Damage);
+    		}
+    	}
+    }
+    protected virtual void ApplyDamage(float Damage){
+    	if(_Health > 0){
+    		_Health -= Damage;
+    		OnDamage();
+	    	if(_Health <= 0){
+	    		Destroy();
+	    	}
+    	}  
+    }
+    protected virtual void OnDamage(){
+
+    }
+    protected virtual void Destroy(){
+    	
+    }
+}
---
+++ Towers_VR/Assets/FracturingObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FracturingObject : DestructableObject
+{
+	[SerializeField] Object FracturedObject;
+	protected override void Destroy(){
+		DisableObject();
+		SpawnFracturedObject();
+	}
+	protected virtual void DisableObject(){
+		gameObject.SetActive(false);
+	}
+	protected virtual void SpawnFracturedObject(){
+		Object.Instantiate(FracturedObject, gameObject.transform.position, gameObject.transform.rotation, gameObject.transform.parent);
+	}
+}
