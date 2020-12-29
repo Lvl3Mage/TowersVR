f38623fcce2fd8c734a61afd32666087480f4668
Lvl3Mage <lvlthreemage@gmail.com>
2020-12-29 17:22:35 +0100

    Configured the damage on destructable objects
    Change some parameters on the destructable objects to better suit the game

--- Towers_VR/Assets/AnimationTogglerButton.cs
+++ Towers_VR/Assets/AnimationTogglerButton.cs
@@ -6,15 +6,9 @@ public class AnimationTogglerButton : SingleActivatedButton
 {
 	[SerializeField] string BoolName = "State";
 	[SerializeField] Animator TargetAnimator;
-	[SerializeField] bool InitialState = false;
-	bool State;
-	void Start(){
-		State = InitialState;
-	}
     // Start is called before the first frame update
 	protected override void SingleActivation(){
-		State = !State;
-		TargetAnimator.SetBool(BoolName, State);
+		TargetAnimator.SetBool(BoolName, !TargetAnimator.GetBool(BoolName)); // toggles the animator bool
 		
 	}
 }
