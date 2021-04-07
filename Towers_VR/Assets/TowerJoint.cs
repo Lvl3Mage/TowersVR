e5aab14233b487590684c18733fdee3f397a5725
Lvl3Mage <lvlthreemage@gmail.com>
2021-04-07 05:04:22 +0200

    Rewritten the automatically connecting joint all towers are built with. Fixed cannon breaking off bug
    Rewritten the automatic joint class and named it appropriately. Also created another joint prefab to hold the cannon and prevent it from breaking off.

---
+++ Towers_VR/Assets/TowerJoint.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerJoint : MonoBehaviour
+{
+	[Header("Joint Settings")]
+
+	[Tooltip("The pointer towards the direction which the joint should form towards")]
+	[SerializeField] Transform Pointer;
+
+	[Tooltip("Enables collision between connected objects")]
+	[SerializeField] bool EnableObjectCollision;
+
+	[Tooltip("The force after which the joint should break")]
+	[SerializeField] float BreakForce;
+
+	[Tooltip("The torque after which the joint should break")]
+	[SerializeField] float  BreakTorque;
+
+	[SerializeField] float MaxDistance = 0.75f;
+
+
+	[Header("Gizmo Settings")]
+	[SerializeField] bool DrawGizmos;
+	[SerializeField] float GizmoSize;
+	[SerializeField] Color MainColor;
+	[SerializeField] Color PointerColor;
+    
+    void Start()
+    {
+        GameObject Followed = transform.parent.gameObject;
+        GameObject Follower = null;
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, Pointer.position-transform.position, out hit, MaxDistance)){
+            if(hit.collider.gameObject != Followed){
+                Follower = hit.collider.gameObject;
+            }
+        }
+        if(Followed && Follower){ // Checks if the conncetion objects exist
+        	Rigidbody followerRB = Follower.GetComponent<Rigidbody>();
+        	Rigidbody followedRB = Followed.GetComponent<Rigidbody>();
+        	if(followedRB && followerRB){
+	        	ConfigurableJoint CJ = Followed.AddComponent<ConfigurableJoint>();
+	            CJ.connectedBody = Follower.GetComponent<Rigidbody>();
+	            CJ.autoConfigureConnectedAnchor = false;
+	            CJ.anchor = Followed.transform.InverseTransformPoint(transform.position);
+	            CJ.connectedAnchor = Follower.transform.InverseTransformPoint(transform.position);
+
+
+	            CJ.xMotion = ConfigurableJointMotion.Limited;
+	            CJ.yMotion = ConfigurableJointMotion.Limited;
+	            CJ.zMotion = ConfigurableJointMotion.Limited;
+	            CJ.angularXMotion = ConfigurableJointMotion.Limited;
+	            CJ.angularYMotion = ConfigurableJointMotion.Limited;
+	            CJ.angularZMotion = ConfigurableJointMotion.Limited;
+
+
+	            CJ.breakForce = BreakForce;
+	            CJ.breakTorque = BreakTorque;
+	            CJ.enableCollision = EnableObjectCollision;
+        	}
+        }
+        Destroy(gameObject);
+    }
+    void OnDrawGizmos()
+    {
+    	if(DrawGizmos){
+    		Gizmos.color = MainColor;
+    		Gizmos.DrawSphere(transform.position, GizmoSize);
+    		Gizmos.color = PointerColor;
+            Gizmos.DrawSphere(Pointer.position, GizmoSize/2f);
+    	}
+    }
+}
